c83b5dbd5abc1f95adc2864e23165b0af631afe8
Yogesh-Chandra-007 <talluriyogeshchandra@gmail.com>
2025-09-15 09:40:30 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,20 @@
-// firebase-config.js
+// firebase-config.js (PERFECT AS IS!)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkMF2TLoWN-slOPx9uerJMY8D08yP9WK4",  // ✅ your key
-  authDomain: "sc-database-167c0.firebaseapp.com",   // 👈 usually projectId + ".firebaseapp.com"
-  databaseURL: "https://sc-database-167c0-default-rtdb.firebaseio.com", // 👈 DB URL from console
-  projectId: "sc-database-167c0",                   // ✅ your projectId
-  storageBucket: "sc-database-167c0.appspot.com",   // 👈 usually projectId + ".appspot.com"
-  messagingSenderId: "233077157231",              // 🔴 copy from console
-  appId: "1:233077157231:web:027988df72c8c20a288c9a"                              // 🔴 copy from console
+  apiKey: "AIzaSyCkMF2TLoWN-slOPx9uerJMY8D08yP9WK4",
+  authDomain: "sc-database-167c0.firebaseapp.com",
+  databaseURL: "https://sc-database-167c0-default-rtdb.firebaseio.com",
+  projectId: "sc-database-167c0",
+  storageBucket: "sc-database-167c0.appspot.com", // This can stay, it won't cost anything
+  messagingSenderId: "233077157231",
+  appId: "1:233077157231:web:027988df72c8c20a288c9a"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
-
+// No storage export needed - we're using Base64
